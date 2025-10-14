22398b2aa8807e92b53a7ac5cde3cfc737661bf7
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-14 23:01:29 +0100

    feat: Direction animated in mutable state.

--- src/components/Direction.tsx
+++ src/components/Direction.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Button } from '@/components/ui/button.tsx'
 import { useNavigate, useParams } from 'react-router'
 
@@ -7,34 +6,27 @@ interface DirectionProps {
     mutable?: boolean
 }
 
-const DIRECTION_LEFT = 'left'
-const DIRECTION_RIGHT = 'right'
-
-type DirectionType = typeof DIRECTION_LEFT | typeof DIRECTION_RIGHT
-
 const Direction = (props: DirectionProps) => {
     const { className = 'text-6xl', mutable = false } = props
 
     const { start_player_id: startPlayerId, end_player_id: endPlayerId } = useParams()
     const navigate = useNavigate()
-    const [direction, setDirection] = useState<DirectionType>(DIRECTION_RIGHT)
 
     // TODO: Ask confirmation
     const onClick = () => {
         if (!mutable) return
 
-        setDirection(direction === DIRECTION_LEFT ? DIRECTION_RIGHT : DIRECTION_LEFT)
         navigate(`/play/${endPlayerId}/${startPlayerId}`)
     }
 
     const arrow = () => {
-        return <span className={className}>{direction === DIRECTION_LEFT ? '→' : '→'}</span>
+        return <span className={className}>→</span>
     }
 
     return (
         <div>
             {mutable ? (
-                <Button variant="ghost" onClick={onClick}>
+                <Button className="hover:rotate-180" variant="ghost" onClick={onClick}>
                     {arrow()}
                 </Button>
             ) : (
