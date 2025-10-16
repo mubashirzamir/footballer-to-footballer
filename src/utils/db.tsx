b4c3515dafea18aa60fcf0b3c8194cc04fc4af40
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-16 18:00:35 +0100

    feat: Added games for 2 more days.

--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,6 +1,20 @@
 import type { Game } from '@/structures'
 
 export const games: Record<string, Game> = {
+    '2025-10-18': {
+        start_player_id: '3111',
+        start_player_name: 'Zinedine Zidane',
+        end_player_id: '3455',
+        end_player_name: 'Zlatan Ibrahimović',
+        suggested_by: 'mushi',
+    },
+    '2025-10-17': {
+        start_player_id: '8198',
+        start_player_name: 'Cristiano Ronaldo',
+        end_player_id: '28003',
+        end_player_name: 'Lionel Messi',
+        suggested_by: 'mushi',
+    },
     '2025-10-16': {
         start_player_id: '7767',
         start_player_name: 'Fernando Torres',
