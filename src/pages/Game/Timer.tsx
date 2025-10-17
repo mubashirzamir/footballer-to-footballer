1146bc7f8aa6772a7fc6114c1fc19dc6a4e8c971
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-17 19:13:16 +0100

    feat: Responsive game summary.

--- src/pages/Game/Timer.tsx
+++ src/pages/Game/Timer.tsx
@@ -1,7 +1,7 @@
 import Text from '@/components/Text.tsx'
 
 const Timer = ({ time }: { time: number }) => {
-    return <Text text={`${time}s`} className="font-bold md:text-2xl sm:text-xl" />
+    return <Text text={`${time}s`} className="font-bold md:text-2xl text-xl" />
 }
 
 export default Timer
