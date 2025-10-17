759693b973e8a7dd63066c721316c98bf7bf092d
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-18 00:53:58 +0100

    fix: Better error component.

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,25 +1,27 @@
-import type { ReactNode } from 'react'
 import { ENV_IS_DEV } from '@/utils/constants.ts'
 import Text from '@/components/Text.tsx'
+import { __ } from '@/lang/lang.ts'
+import { JSX } from 'react'
+import { Logger } from '@/utils'
 
 interface ErrorProps {
     error: unknown
 }
 
 const Error = ({ error }: ErrorProps) => {
-    let message: ReactNode = (
-        // @ts-ignore // TODO
-        <span className="whitespace-pre-wrap">{error?.message || String(error) || 'An unknown error occurred'}</span>
-    )
+    let message: JSX.Element = <>{__.errors.error_for_user}</>
 
-    if (!ENV_IS_DEV) message = 'An error occurred. Please try again later.'
+    if (ENV_IS_DEV) {
+        // @ts-ignore
+        message = <>{error?.message || String(error) || __.errors.unknown_error}</>
+    }
+
+    Logger.log('Error: ', error)
 
     return (
         <div className="flex flex-col items-center justify-center min-h-[200px]">
             <div className="p-4 bg-red-100 text-red-800 border border-red-400 rounded">
-                <Text className="block sm:inline ml-2">
-                    {message}
-                </Text>
+                <Text className="block sm:inline ml-2">{message}</Text>
             </div>
         </div>
     )
