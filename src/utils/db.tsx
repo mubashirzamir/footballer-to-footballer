e27dbebe5f80e834c750cceb84b1fd1dbfe45105
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-20 22:32:02 +0100

    feat: Games.

--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,6 +1,41 @@
 import type { Game } from '@/structures'
 
 export const GAMES: Record<string, Game> = {
+    '2025-10-25': {
+        start_player_id: '37148',
+        start_player_name: 'Gabriel Agbonlahor',
+        end_player_id: '503733',
+        end_player_name: 'Anthony Gordon',
+        suggested_by: 'mushi',
+    },
+    '2025-10-24': {
+        start_player_id: '26399',
+        start_player_name: 'Sergio Agüero',
+        end_player_id: '31095',
+        end_player_name: 'Nuri Şahin',
+        suggested_by: 'mushi',
+    },
+    '2025-10-23': {
+        start_player_id: '50202',
+        start_player_name: 'Eden Hazard',
+        end_player_id: '418560',
+        end_player_name: 'Erling Haaland',
+        suggested_by: 'mushi',
+    },
+    '2025-10-22': {
+        start_player_id: '342229',
+        start_player_name: 'Kylian Mbappé',
+        end_player_id: '12000',
+        end_player_name: 'Éric Cantona',
+        suggested_by: 'mushi',
+    },
+    '2025-10-21': {
+        start_player_id: '3366',
+        start_player_name: 'Kaká',
+        end_player_id: '937958',
+        end_player_name: 'Lamine Yamal',
+        suggested_by: 'mushi',
+    },
     '2025-10-20': {
         start_player_id: '50202',
         start_player_name: 'Eden Hazard',
