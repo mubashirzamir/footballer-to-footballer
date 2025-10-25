c8b901be83ebd269cfae57a87510b8cfd08371f9
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-25 23:02:29 +0100

    chore: Added contribution guide link on no game page.

--- src/pages/NoGame/index.tsx
+++ src/pages/NoGame/index.tsx
@@ -5,6 +5,14 @@ const NoGame = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4">
             <Text className="md:text-3xl sm:text-2xl text-xl font-bold mb-4">{__.errors.no_game_today}</Text>
+            <Text className="md:text-3xl sm:text-2xl text-xl italic underline mb-4">
+                <a
+                    href="https://github.com/mubashirzamir/footballer-to-footballer/blob/main/src/utils/CONTRIBUTION_GUIDE.md"
+                    target="_blank"
+                >
+                    {__.messages.home.how_to_contribute}
+                </a>
+            </Text>
         </div>
     )
 }
